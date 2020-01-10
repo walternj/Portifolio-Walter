d385e8ad603c9dad5db76cec0e7ec6c5f29c9019
walternj <43467311+walternj@users.noreply.github.com>
2020-01-10 17:44:09 +0100

    Updates

---
+++ precache-manifest.909fb8c60b8fcb3549ab3ba74fa51e6a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849ea2b308a301e1e22acb6dbbfdfe15",
+    "url": "/portifolio-walternj/index.html"
+  },
+  {
+    "revision": "95e10238138ed52cbec1",
+    "url": "/portifolio-walternj/static/css/2.216ee72c.chunk.css"
+  },
+  {
+    "revision": "dbdec213f7f44deb065a",
+    "url": "/portifolio-walternj/static/css/main.b634272e.chunk.css"
+  },
+  {
+    "revision": "95e10238138ed52cbec1",
+    "url": "/portifolio-walternj/static/js/2.b6c189eb.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/portifolio-walternj/static/js/2.b6c189eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbdec213f7f44deb065a",
+    "url": "/portifolio-walternj/static/js/main.2c7c8f14.chunk.js"
+  },
+  {
+    "revision": "8c263f5b1525a08f373b",
+    "url": "/portifolio-walternj/static/js/runtime-main.d8c9af69.js"
+  },
+  {
+    "revision": "b464889840d2a96a5dc30059d9f4ad9a",
+    "url": "/portifolio-walternj/static/media/DancingScript-Regular.b4648898.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/portifolio-walternj/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/portifolio-walternj/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/portifolio-walternj/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f49f3d2d9df5013c9bfaab7e3d39ee57",
+    "url": "/portifolio-walternj/static/media/Raleway-Bold.f49f3d2d.ttf"
+  },
+  {
+    "revision": "466d154fedd98f85c9deb363ccf859a7",
+    "url": "/portifolio-walternj/static/media/Raleway-Light.466d154f.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/portifolio-walternj/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "a68755ff0c6b4464fbdeb3a8f64a4b1c",
+    "url": "/portifolio-walternj/static/media/WIN_20180320_09_40_06_Pro.a68755ff.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portifolio-walternj/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portifolio-walternj/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portifolio-walternj/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portifolio-walternj/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portifolio-walternj/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "86dcf0545f09c0b60390fef1ffa70b86",
+    "url": "/portifolio-walternj/static/media/responsive.86dcf054.jfif"
+  },
+  {
+    "revision": "885f52fb17cd1423d67f5ffd5310fc24",
+    "url": "/portifolio-walternj/static/media/restaurant-savoureux.885f52fb.png"
+  },
+  {
+    "revision": "330a759078940ab3132888aca6e02543",
+    "url": "/portifolio-walternj/static/media/topmovies.330a7590.png"
+  },
+  {
+    "revision": "8235de7f8578dedf76a0da45a1bca2c3",
+    "url": "/portifolio-walternj/static/media/topmovies.8235de7f.jpg"
+  }
+]);
