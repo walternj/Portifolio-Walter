3b97b849590e0ee544ac558479c2337b3b93e73e
Walter Nunes Jr <43467311+walternj@users.noreply.github.com>
2020-05-18 11:08:12 +0200

    Add files via upload
    build 1.1

---
+++ build/precache-manifest.ebc38cbd0e36318b2491e8e9b563bab8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f336a4bf30ab53500fc0d41609a48c20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "475454db07448eef297c",
+    "url": "/static/css/2.03fe6687.chunk.css"
+  },
+  {
+    "revision": "27e4802d97b3c657a12c",
+    "url": "/static/css/main.c37392d9.chunk.css"
+  },
+  {
+    "revision": "475454db07448eef297c",
+    "url": "/static/js/2.fe71b78a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.fe71b78a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27e4802d97b3c657a12c",
+    "url": "/static/js/main.d7b51c17.chunk.js"
+  },
+  {
+    "revision": "60e1d4c127343217c313",
+    "url": "/static/js/runtime-main.59a0600b.js"
+  },
+  {
+    "revision": "b464889840d2a96a5dc30059d9f4ad9a",
+    "url": "/static/media/DancingScript-Regular.b4648898.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f49f3d2d9df5013c9bfaab7e3d39ee57",
+    "url": "/static/media/Raleway-Bold.f49f3d2d.ttf"
+  },
+  {
+    "revision": "466d154fedd98f85c9deb363ccf859a7",
+    "url": "/static/media/Raleway-Light.466d154f.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "0055bf466835890bce6feefcb11f1a9a",
+    "url": "/static/media/RestApi.0055bf46.png"
+  },
+  {
+    "revision": "ef10fb8692389e15168ec85c5c1a3f71",
+    "url": "/static/media/WalterDev.ef10fb86.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3eaad7a97c23da4b75c6fac7f0fe53cf",
+    "url": "/static/media/mytopmovies.3eaad7a9.jpg"
+  },
+  {
+    "revision": "d3a5abca43edb93b8497f1aef290c0ad",
+    "url": "/static/media/photo.d3a5abca.jpg"
+  },
+  {
+    "revision": "86dcf0545f09c0b60390fef1ffa70b86",
+    "url": "/static/media/responsive.86dcf054.jfif"
+  },
+  {
+    "revision": "885f52fb17cd1423d67f5ffd5310fc24",
+    "url": "/static/media/restaurant-savoureux.885f52fb.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portifolio-walter/precache-manifest.e10be693719b58ac50c40213b2b6a52c.js"
+  "/precache-manifest.ebc38cbd0e36318b2491e8e9b563bab8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portifolio-walter/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
